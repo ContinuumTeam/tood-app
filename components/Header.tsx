8fa9b0026f23c63a450ac3a74e58a40b8cbff8eb
MatheusFontenele <matheus.fontenele@icloud.com>
2022-09-17 20:33:08 -0300

    feat(header): create header component

---
+++ components/Header.tsx
@@ -0,0 +1,70 @@
+import { ReactNode } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+import { FontAwesome } from "@expo/vector-icons";
+import avatar from "../assets/images/avatar.jpeg";
+import { RectButton } from "react-native-gesture-handler";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+export function Header() {
+	return (
+		<View style={styles.container}>
+			<View style={styles.profileArea}>
+				<Image source={avatar} style={styles.avatar} />
+				<View style={styles.textArea}>
+					<Text style={styles.title}>Hi, Matheus!</Text>
+					<Text style={styles.subtitle}>You have 24 tasks</Text>
+				</View>
+			</View>
+			<View style={styles.settingsArea}>
+				<RectButton style={styles.calendarButton}>
+					<FontAwesome name="calendar" size={25} color="#FFF" />
+				</RectButton>
+				<RectButton>
+					<FontAwesome name="bell-o" size={25} color="#FFF" />
+				</RectButton>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		marginTop: getStatusBarHeight() + 30,
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		paddingHorizontal: 24,
+	},
+	profileArea: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	textArea: {
+		justifyContent: "center",
+		marginLeft: 12,
+	},
+	title: {
+		fontWeight: "bold",
+		fontSize: 28,
+		color: "#fff",
+	},
+	subtitle: {
+		fontSize: 14,
+		color: "#c4c4c4",
+	},
+	avatar: {
+		width: 60,
+		height: 60,
+		borderRadius: 50,
+	},
+	settingsArea: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	calendarButton: {
+		marginRight: 16,
+	},
+});
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,56 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { Header } from "../components";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+export default function TabOneScreen({
+	navigation,
+}: RootTabScreenProps<"TabOne">) {
+	return (
+		<View style={styles.container}>
+			<Header />
+			<View
+				style={styles.separator}
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+			<RectButton
+				style={styles.buttonRect}
+				onPress={() => navigation.navigate("Modal")}
+			>
+				<Text style={styles.buttonText}>testesss</Text>
+			</RectButton>
+			<EditScreenInfo path="/screens/TabOneScreen.tsx" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
+	buttonRect: {
+		width: 120,
+		height: 50,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#fff",
+		borderRadius: 8,
+		marginBottom: 8,
+	},
+	buttonText: {
+		color: "#000",
+	},
 });
